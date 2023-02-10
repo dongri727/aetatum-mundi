9b14526472caef91b170c25a4cd1d67c4b2d77e9
laporte727 <ecole.la.porte@gmail.com>
2023-02-10 16:47:25 +0900

    main.dart remake

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'cover_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft,DeviceOrientation.landscapeRight]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aetatum Mundi',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.grey,
+        brightness: Brightness.dark,
+      ),
+      home: const CoverPage(),
+    );
+  }
+}
